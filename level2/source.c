47112d94e09965aa8762bfe4603fed3e894b30a6
Alban <alban.lfbv@gmail.com>
2021-02-24 11:38:06 +0100

    level2

---
+++ level2/source.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void p(void)
+{
+	// 0x080484d4 <+0>:		push   ebp
+	// 0x080484d5 <+1>:		mov    ebp,esp
+
+	char buf[76];
+	// 0x080484d7 <+3>:		sub    esp,0x68
+
+	fflush(stdout);
+	// 0x080484da <+6>:		mov    eax,ds:0x8049860
+	// 0x080484df <+11>:	mov    DWORD PTR [esp],eax
+	// 0x080484e2 <+14>:	call   0x80483b0 <fflush@plt>
+
+	gets(buf);
+	// 0x080484e7 <+19>:	lea    eax,[ebp-0x4c]
+	// 0x080484ea <+22>:	mov    DWORD PTR [esp],eax
+	// 0x080484ed <+25>:	call   0x80483c0 <gets@plt>
+
+	buf[ebp - 0xc] = ebp + 0x4;
+	// 0x080484f2 <+30>:	mov    eax,DWORD PTR [ebp+0x4]
+	// 0x080484f5 <+33>:	mov    DWORD PTR [ebp-0xc],eax
+
+	if (buf[ebp - 0xc] & 0xb0000000 == 0xb0000000)
+	{
+		printf("(%p)\n", buf[ebp - 0xc]);
+		exit(1);
+	}
+	// 0x080484f8 <+36>:	mov    eax,DWORD PTR [ebp-0xc]
+	// 0x080484fb <+39>:	and    eax,0xb0000000
+	// 0x08048500 <+44>:	cmp    eax,0xb0000000
+	// 0x08048505 <+49>:	jne    0x8048527 <p+83>
+	// 0x08048507 <+51>:	mov    eax,0x8048620
+	// 0x0804850c <+56>:	mov    edx,DWORD PTR [ebp-0xc]
+	// 0x0804850f <+59>:	mov    DWORD PTR [esp+0x4],edx
+	// 0x08048513 <+63>:	mov    DWORD PTR [esp],eax
+	// 0x08048516 <+66>:	call   0x80483a0 <printf@plt>
+	// 0x0804851b <+71>:	mov    DWORD PTR [esp],0x1
+	// 0x08048522 <+78>:	call   0x80483d0 <_exit@plt>
+
+	puts(buf);
+	strdup(buf);
+	// 0x08048527 <+83>:	lea    eax,[ebp-0x4c]
+	// 0x0804852a <+86>:	mov    DWORD PTR [esp],eax
+	// 0x0804852d <+89>:	call   0x80483f0 <puts@plt>
+	// 0x08048532 <+94>:	lea    eax,[ebp-0x4c]
+	// 0x08048535 <+97>:	mov    DWORD PTR [esp],eax
+	// 0x08048538 <+100>:	call   0x80483e0 <strdup@plt>
+	// 0x0804853d <+105>:	leave
+	// 0x0804853e <+106>:	ret
+}
+
+int main(void)
+{
+
+	//0x0804853f <+0>:	push   ebp
+	//0x08048540 <+1>:	mov    ebp,esp
+	//0x08048542 <+3>:	and    esp,0xfffffff0
+	p();
+	return (0);
+	//0x08048545 <+6>:	call   0x80484d4 <p>
+	//0x0804854a <+11>:	leave
+	//0x0804854b <+12>:	ret
+}
